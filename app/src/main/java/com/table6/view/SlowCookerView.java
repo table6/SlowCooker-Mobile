e410c67a796831253513120c2b520af9ec25d98b
Jonathon Gebhardt <jonathon.gebhardt@gmail.com>
2019-04-09 13:48:28 -0400

    Add view pager.

--- app/src/main/java/com/table6/view/SlowCookerView.java
+++ app/src/main/java/com/table6/view/SlowCookerView.java
@@ -4,12 +4,16 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import com.table6.activity.R;
@@ -30,6 +34,8 @@ import java.net.URL;
 
 public class SlowCookerView extends AppCompatActivity {
 
+    FragmentPagerAdapter fragmentPagerAdapter;
+
     private ControlCookTimeFragment controlCookTimeFragment;
     private ControlCookModeRadioFragment controlCookModeFragment;
     private ControlTemperatureFragment controlTemperatureFragment;
@@ -40,80 +46,113 @@ public class SlowCookerView extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_slowcooker_view);
 
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        ViewPager viewPager = (ViewPager) findViewById(R.id.slowCookerViewViewPager);
+        fragmentPagerAdapter = new SlowCookerPagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(fragmentPagerAdapter);
 
-        controlCookModeFragment = ControlCookModeRadioFragment.newInstance();
-        fragmentTransaction.add(R.id.slowCookerViewFragmentContainer, controlCookModeFragment);
-
-        controlCookTimeFragment = ControlCookTimeFragment.newInstance();
-        fragmentTransaction.add(R.id.slowCookerViewFragmentContainer, controlCookTimeFragment);
-
-        SharedPreferences sharedPreferences = getSharedPreferences(getString(R.string.preference_file_key), Context.MODE_PRIVATE);
-        String type = sharedPreferences.getString(getString(R.string.preference_file_type), "");
-        if (type.equals("probe")) {
-            controlTemperatureFragment = ControlTemperatureFragment.newInstance();
-            fragmentTransaction.add(R.id.slowCookerViewFragmentContainer, controlTemperatureFragment);
-        } else {
-            controlTemperatureRadioFragment = ControlTemperatureRadioFragment.newInstance();
-            fragmentTransaction.add(R.id.slowCookerViewFragmentContainer, controlTemperatureRadioFragment);
-        }
-
-        fragmentTransaction.commit();
-
-        Button confirmBtn = (Button) findViewById(R.id.slowCookerViewConfirmBtn);
-        confirmBtn.setOnClickListener(new View.OnClickListener() {
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
             @Override
-            public void onClick(View v) {
+            public void onPageScrolled(int i, float v, int i1) {
 
-                ServerFeed cookTimeFeed = new ServerFeed();
-                if (controlCookTimeFragment != null) {
-                    String cookTime = controlCookTimeFragment.getCookTime();
-                    if(cookTime.length() > 0) {
-                        JSONObject json = new JSONObject();
-                        try {
-                            json.put("start_time", cookTime);
-                            cookTimeFeed = new ServerFeed("control_cook_time", json);
-                        } catch (JSONException e) {
-                            Log.e("", e.getMessage());
-                        }
-                    }
-                }
+            }
 
-                ServerFeed temperatureFeed = new ServerFeed();
-                if (controlTemperatureFragment != null) {
-                    String temperature = controlTemperatureFragment.getTemperature();
-                    if (temperature.length() > 0) {
-                        JSONObject json = new JSONObject();
-                        try {
-                            json.put("type", "probe");
-                            json.put("temperature", temperature);
-                            json.put("measurement", "F");
-                            temperatureFeed = new ServerFeed("control_temperature", json);
-                        } catch (JSONException e) {
-                            Log.e("", e.getMessage());
-                        }
-                    }
-                }
+            @Override
+            public void onPageSelected(int i) {
+                // Called when the page changes
 
-                if (controlTemperatureRadioFragment != null) {
-                    String heatMode = controlTemperatureRadioFragment.getHeatMode();
-                    if (heatMode.length() > 0) {
-                        JSONObject json = new JSONObject();
-                        try {
-                            json.put("type", "manual");
-                            json.put("temperature", heatMode);
-                            json.put("measurement", "N/A");
-                            temperatureFeed = new ServerFeed("control_temperature", json);
-                        } catch (JSONException e) {
-                            Log.e("", e.getMessage());
-                        }
-                    }
-                }
+            }
 
-                new PushFeedTask().execute(cookTimeFeed, temperatureFeed);
+            @Override
+            public void onPageScrollStateChanged(int i) {
+
             }
         });
+
+//        FragmentManager fragmentManager = getSupportFragmentManager();
+//        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+//
+//        controlCookModeFragment = ControlCookModeRadioFragment.newInstance();
+//        fragmentTransaction.add(R.id.slowCookerViewFragmentContainer, controlCookModeFragment);
+//
+//        controlCookTimeFragment = ControlCookTimeFragment.newInstance();
+//        fragmentTransaction.add(R.id.slowCookerViewFragmentContainer, controlCookTimeFragment);
+//
+//        SharedPreferences sharedPreferences = getSharedPreferences(getString(R.string.preference_file_key), Context.MODE_PRIVATE);
+//        String type = sharedPreferences.getString(getString(R.string.preference_file_type), "");
+//        if (type.equals("probe")) {
+//            controlTemperatureFragment = ControlTemperatureFragment.newInstance();
+//            fragmentTransaction.add(R.id.slowCookerViewFragmentContainer, controlTemperatureFragment);
+//        } else {
+//            controlTemperatureRadioFragment = ControlTemperatureRadioFragment.newInstance();
+//            fragmentTransaction.add(R.id.slowCookerViewFragmentContainer, controlTemperatureRadioFragment);
+//        }
+//
+//        fragmentTransaction.commit();
+//
+//        Button confirmBtn = (Button) findViewById(R.id.slowCookerViewConfirmBtn);
+//        confirmBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//                ServerFeed cookTimeFeed = new ServerFeed();
+//                if (controlCookTimeFragment != null) {
+//                    String cookTime = controlCookTimeFragment.getCookTime();
+//                    if(cookTime.length() > 0) {
+//                        JSONObject json = new JSONObject();
+//                        try {
+//                            json.put("start_time", cookTime);
+//                            cookTimeFeed = new ServerFeed("control_cook_time", json);
+//                        } catch (JSONException e) {
+//                            Log.e("", e.getMessage());
+//                        }
+//                    }
+//                }
+//
+//                ServerFeed temperatureFeed = new ServerFeed();
+//                if (controlTemperatureFragment != null) {
+//                    String temperature = controlTemperatureFragment.getTemperature();
+//                    if (temperature.length() > 0) {
+//                        JSONObject json = new JSONObject();
+//                        try {
+//                            json.put("type", "probe");
+//                            json.put("temperature", temperature);
+//                            json.put("measurement", "F");
+//                            temperatureFeed = new ServerFeed("control_temperature", json);
+//                        } catch (JSONException e) {
+//                            Log.e("", e.getMessage());
+//                        }
+//                    }
+//                } else if (controlTemperatureRadioFragment != null) {
+//                    String heatMode = controlTemperatureRadioFragment.getHeatMode();
+//                    if (heatMode.length() > 0) {
+//                        JSONObject json = new JSONObject();
+//                        try {
+//                            json.put("type", "manual");
+//                            json.put("temperature", heatMode);
+//                            json.put("measurement", "N/A");
+//                            temperatureFeed = new ServerFeed("control_temperature", json);
+//                        } catch (JSONException e) {
+//                            Log.e("", e.getMessage());
+//                        }
+//                    }
+//                }
+//
+//                if (controlCookModeFragment != null) {
+//                    String heatMode = controlTemperatureRadioFragment.getHeatMode();
+//                    if (heatMode.length() > 0) {
+//                        JSONObject json = temperatureFeed.getJson();
+//                        try {
+//                            json.put("type", "manual");
+//                            temperatureFeed = new ServerFeed("control_temperature", json);
+//                        } catch (JSONException e) {
+//                            Log.e("", e.getMessage());
+//                        }
+//                    }
+//                }
+//
+//                new PushFeedTask().execute(cookTimeFeed, temperatureFeed);
+//            }
+//        });
     }
 
     public class PushFeedTask extends AsyncTask<ServerFeed, Void, Void> {
@@ -161,4 +200,41 @@ public class SlowCookerView extends AppCompatActivity {
             return null;
         }
     }
+
+    public class SlowCookerPagerAdapter extends FragmentPagerAdapter {
+
+        private int NUM_ITEMS = 4;
+        private int PAGE_COOK_MODE = 0;
+        private int PAGE_COOK_TIME = 1;
+        private int PAGE_COOK_TEMPERATURE = 2;
+        private int PAGE_COOK_TEMPERATURE_RADIO = 3;
+
+        public SlowCookerPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int i) {
+            switch(i) {
+                case 0:
+                    return ControlCookModeRadioFragment.newInstance();
+                case 1:
+                    return ControlCookTimeFragment.newInstance();
+                case 2:
+                    return ControlTemperatureFragment.newInstance();
+                case 3:
+                    return ControlTemperatureRadioFragment.newInstance();
+                default:
+                    break;
+            }
+
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return NUM_ITEMS;
+        }
+    }
+
 }
